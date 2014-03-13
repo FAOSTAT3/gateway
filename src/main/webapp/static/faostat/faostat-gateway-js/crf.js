6a3a1e5aad40071c195c00ab9f7602462e8da649
Kalimaha <guido.barbaglia@fao.org>
2014-03-13 12:08:55 +0100

    CRF.js

---
+++ src/main/webapp/static/faostat/faostat-gateway-js/crf.js
@@ -0,0 +1,21 @@
+var CRF = (function() {
+
+    var CONFIG = {
+
+    };
+
+    function showCRF1996() {
+        alert('1996');
+    };
+
+    function showCRF2006() {
+        alert('2006');
+    };
+
+    return {
+        CONFIG      :   CONFIG,
+        showCRF1996 :   showCRF1996,
+        showCRF2006 :   showCRF2006
+    };
+
+})();
