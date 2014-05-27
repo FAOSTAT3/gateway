5fdf6b252091498c06e5fec57c95d07191c333e8
vortex <simone.murzilli@fao.org>
2014-05-27 11:58:00 +0200

    database update

--- src/main/webapp/static/faostat/faostat-home-js/database-updates.js
+++ src/main/webapp/static/faostat/faostat-home-js/database-updates.js
@@ -5,7 +5,7 @@ if (!window.FAOSTATDatabaseUpdate) {
         getDatabaseUpdates: function(db, lang) {
 
             var q = {};
-            q.query = "SELECT TOP 10 groupcode, groupname" + lang + ", domaincode, domainname" + lang + ", dateupdate " +
+            q.query = "SELECT TOP 10 groupcode, groupname" + lang + ", domaincode, domainname" + lang + ", DATENAME(mm, dateupdate) AS Month,  DATENAME(yyyy, dateupdate) AS Year  " +
                       "FROM Domain " +
                       "ORDER BY dateupdate DESC ";
             var data = {};
@@ -15,13 +15,12 @@ if (!window.FAOSTATDatabaseUpdate) {
             data.decimalNumbers = this.decimalValues;
             data.json = JSON.stringify(q);
 
-
-
             var _this = this;
             //console.log(data);
             $.ajax({
                 type : 'POST',
-                url : 'http://' + CORE.baseURL + '/wds/rest/table/json',
+//                url : 'http://' + CORE.baseURL + '/wds/rest/table/json',
+                url : 'http://168.202.28.214:8090/wds/rest/table/json',
                 data : data,
                 success : function(response) {
 
@@ -29,15 +28,24 @@ if (!window.FAOSTATDatabaseUpdate) {
                         response = $.parseJSON(response);
 
 
-                   CORE.getLangProperties();
+                   //CORE.getLangProperties();
 
                    var html = '';
                     html += '<ul>';
+                    var month = ""
+                    var year = ""
                     for (var i = 0 ; i < response.length ; i++) {
-                        var parts = response[i][4].match(/(\d+)/g);
-                        var d = parts[0] + '-' + parts[1] + '-' + parts[2];
+                        if (month != response[i][4]) {
+                            // create new div with month and year
+                            month = response[i][4];
+                            year = response[i][5];
+                            // TODO: month multilanguage
+                            html += '<div>'+ month + ' ' + year + '</div>'
+                        }
                         html += '<li id="update_' +response[i][2] +'" class="db-update-item"><b>'+ response[i][3] + "</b> " + d +' (' + response[i][1]  +')</li>';
                     }
+
+
                     html += '</ul>';
                     $("#database-updates").append(html);
                     for (var i = 0 ; i < response.length ; i++) {
